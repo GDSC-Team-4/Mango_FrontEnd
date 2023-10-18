3fd53d1debc07610d9ffb5cdcea8270c02fc3b20
OhHyukJun <kmh7277@naver.com>
2023-10-18 17:55:30 +0900

    feat: inteface change

--- src/Interface/Header.ts
+++ src/Interface/Header.ts
@@ -1,6 +1,7 @@
 interface HeaderProps {
-    backgroundColor: string;
-    opacity: number;
+    backgroundColor: string | undefined;
+    border: string | undefined;
+    boxShadow: string | undefined;
 }
 
 export default HeaderProps
