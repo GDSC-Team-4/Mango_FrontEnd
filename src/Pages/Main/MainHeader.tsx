a99889a226964af4f86010422b08466a0e5ba801
LEEHYEYEONG <elto789@naver.com>
2023-10-18 20:21:03 +0900

    style: 헤더 코드 정렬

--- src/Pages/Main/MainHeader.tsx
+++ src/Pages/Main/MainHeader.tsx
@@ -1,132 +1,137 @@
-import React from 'react';
-import styled from 'styled-components';
-import Rectangle3  from '../../img/Rectangle3.png';
-import {AiOutlineSearch} from "react-icons/ai";
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import Rectangle3 from "../../img/Rectangle3.png";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const MainContainer = styled.header`
-    width: 100%;
-    height: 95vh;
-    background-image: url(${require('../../img/Rectangle.png')});
-    background-size: cover; 
-    background-position:center;
-    display:flex;
-    justify-content:center;
-    align-items:center;
+  width: 100%;
+  height: 95vh;
+  background-image: url(${require("../../img/Rectangle.png")});
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    @media (max-width: 768px) {
-        height: 85vh; 
-        padding-top:15rem; 
-    }
- `;
- 
+  @media (max-width: 768px) {
+    height: 85vh;
+    padding-top: 15rem;
+  }
+`;
+
 const ImageBox = styled.div`
-    width: 45vw;
-    height: 54.5vh;
-    display: flex;
-    justify-content: right;
-    align-items: right;
-    margin-top:1.5vw;
-    margin-left:-15vw;
+  width: 45vw;
+  height: 54.5vh;
+  display: flex;
+  justify-content: right;
+  align-items: right;
+  margin-top: 1.5vw;
+  margin-left: -15vw;
 `;
 
 const Box = styled.div`
-    display: block;
-    margin-left:5vw;
-    margin-top:7vw;
+  display: block;
+  margin-left: 5vw;
+  margin-top: 7vw;
 `;
 
 const TextBox = styled.span`
-    height: 30vh;
-    display: flex;
-    flex-direction: column;
-    align-items: left;
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
 `;
 
 const TitleText = styled.h1`
-    font-family: 'Inter', sans-serif;
-    font-style: normal;
-    font-weight: 800;
-    font-size: 3em; 
-    height:2.5em;
-    line-height:3em;
-    color: #FCFDF2;
+  font-family: "Inter", sans-serif;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 3em;
+  height: 2.5em;
+  line-height: 3em;
+  color: #fcfdf2;
 `;
 
 const SubText = styled.p`
-   font-family : 'Inter', sans-serif ;
-   font-style : normal ;
-   font-weight :400 ;
-   font-size :1.3em ;
-   line-height :1.2em;
-   color : #FCFDF2 ;
+  font-family: "Inter", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.3em;
+  line-height: 1.2em;
+  color: #fcfdf2;
 `;
 
 const SearchContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top:3vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3vw;
 `;
 
 const SearchIcon = styled(AiOutlineSearch)`
   color: #969696;
-  width:3vw;
-  height:5vh;
-  margin-right:-2.7vw;
+  width: 3vw;
+  height: 5vh;
+  margin-right: -2.7vw;
 `;
 
 const SearchBar = styled.input`
-   padding: 1vh; 
-   margin-right:-0.1vw;
-   font-size: 1.4rem;
-   border: none; 
-   border-bottom: 0.1rem solid #777777; 
-   display:flex;
-   float: center;
-   width: 30vw; 
-   height:3.5vh;
-   background: transparent; 
-   text-indent: 2.2vw;
-   color: #777777;
+  padding: 1vh;
+  margin-right: -0.1vw;
+  font-size: 1.4rem;
+  border: none;
+  border-bottom: 0.1rem solid #777777;
+  display: flex;
+  float: center;
+  width: 30vw;
+  height: 3.5vh;
+  background: transparent;
+  text-indent: 2.2vw;
+  color: #777777;
 `;
 
 const SearchButton = styled.button`
-    float:left;
-    width:9vw;
-    height:6vh;
-    background:#767676; 
-    color:white; 
-    font-size:1.3em; 
-    border:none; 
-    cursor:pointer;
-    border-radius:1vh;
-    backdrop-filter: blur(0.5vw);
-    background: rgba(252, 253, 242, 0.2);
+  float: left;
+  width: 9vw;
+  height: 6vh;
+  background: #767676;
+  color: white;
+  font-size: 1.3em;
+  border: none;
+  cursor: pointer;
+  border-radius: 1vh;
+  backdrop-filter: blur(0.5vw);
+  background: rgba(252, 253, 242, 0.2);
 `;
 
-
 export const MainHeader = () => {
-    const navigation = useNavigate();
-    const onClick = () => {
-        navigation('/SearchPage');
-    }
+  const navigation = useNavigate();
+  const onClick = () => {
+    navigation("/SearchPage");
+  };
 
-    return (
-        <MainContainer>
-            <Box>
-                <TextBox>
-                    <TitleText>포도플레이트</TitleText>
-                    <SubText>솔직한 리뷰, 믿을 수 있는 평점!<br/> 포드플레이트에서 나만의 맛집을 찾아보세요.</SubText>
-                </TextBox>
-                <SearchContainer>
-                    <SearchIcon/>
-                    <SearchBar type="text" placeholder="지역, 식당 또는 음식" />
-                    <SearchButton type="submit" onClick={onClick}>검색</SearchButton>
-                </SearchContainer>
-            </Box>
-            <ImageBox><img src={Rectangle3} /></ImageBox> 
-        </MainContainer>
-    );
-}
-
+  return (
+    <MainContainer>
+      <Box>
+        <TextBox>
+          <TitleText>포도플레이트</TitleText>
+          <SubText>
+            솔직한 리뷰, 믿을 수 있는 평점!
+            <br /> 포드플레이트에서 나만의 맛집을 찾아보세요.
+          </SubText>
+        </TextBox>
+        <SearchContainer>
+          <SearchIcon />
+          <SearchBar type="text" placeholder="지역, 식당 또는 음식" />
+          <SearchButton type="submit" onClick={onClick}>
+            검색
+          </SearchButton>
+        </SearchContainer>
+      </Box>
+      <ImageBox>
+        <img src={Rectangle3} />
+      </ImageBox>
+    </MainContainer>
+  );
+};
