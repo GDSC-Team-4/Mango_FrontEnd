427178a0e33fe72176aad6c6b3f039b920b69b3f
OhHyukJun <kmh7277@naver.com>
2023-10-19 17:12:36 +0900

    Feat: 더미 데이터 생성

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -1,9 +1,52 @@
-import React,{useRef} from "react";
+import React,{useRef, useState} from "react";
+import { useRecoilState, useResetRecoilState } from "recoil";
 import styled from "styled-components";
 import {AiOutlineSearch,} from "react-icons/ai";
 import {HiArrowRightCircle} from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 import SearchProps from "../../Interface/Search";
+import { SearchDto } from "../../Interface/Search";
+import { SearchData } from "../../Interface/Search";
+import { searchDataState } from "../../Atom/Search";
+
+const dummyPlaces: SearchDto[] = [
+    {
+        id: 1,
+        placeName: '미성옥',
+        roadAddressName: '서울특별시 마포구 합정동 123-45',
+        placeUrl: 'https://place.map.kakao.com/1234567890',
+        reviewPoint:4.6
+      },
+      {
+        id: 2,
+        placeName: '육전식당',
+        roadAddressName: '서울특별시 마포구 합정동 67-89',
+        placeUrl: 'https://place.map.kakao.com/2345678901',
+        reviewPoint:4.6
+      },
+      {
+          id:3,
+          placeName:'합정떡볶이', 
+          roadAddressName:'서울특별시 마포구 합정동 987-65', 
+          placeUrl:'https://place.map.kakao.com/3456789012',
+          reviewPoint:4.6
+      },
+      {
+          id:4, 
+          placeName:'합정김밥천국', 
+          roadAddressName:'서울특별시 마포구 합정동 543-21', 
+          placeUrl:'https://place.map.kakao.com/4567890123',
+          reviewPoint:4.6
+      },
+      {
+          id:5, 
+          placeName:'합정돈까스집', 
+          roadAddressName:'서울특별시 마포구 합정동 321-54', 
+          placeUrl:'https://place.map.kakao.com/5678901234',
+          reviewPoint:4.6
+      },
+];
+  
 
 const SearchContainer = styled.div`
     width: 100%;
@@ -106,6 +149,7 @@ const ColumnBox = styled.div`
 `;
 
 const RandomRactangle = styled.div<SearchProps>`
+    display: grid;
     width: ${props => props.width}vw;
     height: ${props => props.height}vh;
     background-image: url(${props => props.imageURL});
@@ -114,10 +158,48 @@ const RandomRactangle = styled.div<SearchProps>`
     filter: drop-shadow(0vw 0.3vw 0.65vw rgba(0, 0, 0, 0.1));
 `;
 
+const SubRactangle = styled.div<SearchProps>`
+    display:flex;
+    
+    width: 90%;
+    height: ${props => props.height}vh;
+    align-self: end;
+    justify-self: center;
+    background: rgba(252, 253, 242, 0.7);
+    backdrop-filter: blur(1.5vh);
+    border-radius: 1.4vh;
+    margin-bottom:5%;
+`;
+
+const RactTitle = styled.p`
+    width: 60%;
+    height: 1.7vh;
+    font-family: 'Epilogue';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1rem;
+    line-height: 0.625rem;
+    color: #000000;
+    margin-left:1vw;
+`;
+
+const ReviewPoint = styled.p`
+    width: 30%;
+    height: 1.8vh;
+    text-align:right;
+    font-family: 'Epilogue';
+    font-style: normal;
+    font-weight: 400;
+    color: #7743DB;
+    font-size:1.1rem;
+    line-height:0.325rem;
+`;
+
 export const SearchPage = () => {
     const searchRef = useRef<HTMLInputElement>(null);
     const navigate = useNavigate();
-
+    const [search,setSearch] = useRecoilState(searchDataState);
+    
     const searchButtonClick = (event: React.MouseEvent) => {
         event.stopPropagation();
         navigate('/SearchPage');    
@@ -145,36 +227,72 @@ export const SearchPage = () => {
                         "합정역 맛집"
                     </SubTitle>
                     <RactangleBox>
-                        <RandomRactangle width={24} 
-                                         height={45} imageURL='https://picsum.photos/350/400'/>
+                        <RandomRactangle width={24} height={50} imageURL='https://picsum.photos/400/500'>
+                            <SubRactangle width={0} height={7} imageURL="">
+                                <RactTitle>{dummyPlaces[0].placeName}</RactTitle>
+                                <ReviewPoint>{dummyPlaces[0].reviewPoint}</ReviewPoint>
+                            </SubRactangle>
+                        </RandomRactangle>
                         <ColumnBox>
-                            <RandomRactangle width={21} 
-                                            height={29} imageURL='https://picsum.photos/300/240'/>
-                            <RandomRactangle width={21} 
-                                            height={15} imageURL='https://picsum.photos/300/150'/>
+                            <RandomRactangle width={21} height={33} imageURL='https://picsum.photos/370/240'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>{dummyPlaces[1].placeName}</RactTitle>
+                                    <ReviewPoint>{dummyPlaces[1].reviewPoint}</ReviewPoint>
+                                </SubRactangle>
+                            </RandomRactangle>
+                            <RandomRactangle width={21} height={17} imageURL='https://picsum.photos/370/150'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>{dummyPlaces[2].placeName}</RactTitle>
+                                    <ReviewPoint>{dummyPlaces[2].reviewPoint}</ReviewPoint>
+                                </SubRactangle>
+                            </RandomRactangle>
                         </ColumnBox>
                         <ColumnBox>
-                            <RandomRactangle width={24} 
-                                            height={13} imageURL='https://picsum.photos/350/180'/>
-                            <RandomRactangle width={24} 
-                                            height={32} imageURL='https://picsum.photos/350/300'/>
+                            <RandomRactangle width={24} height={16} imageURL='https://picsum.photos/400/180'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>{dummyPlaces[3].placeName}</RactTitle>
+                                    <ReviewPoint>{dummyPlaces[3].reviewPoint}</ReviewPoint>
+                                </SubRactangle>
+                            </RandomRactangle>
+                            <RandomRactangle width={24} height={34} imageURL='https://picsum.photos/400/300'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>{dummyPlaces[4].placeName}</RactTitle>
+                                    <ReviewPoint>{dummyPlaces[4].reviewPoint}</ReviewPoint>
+                                </SubRactangle>
+                            </RandomRactangle>
                         </ColumnBox>
                     </RactangleBox>
                     <RactangleBox>
                         <ColumnBox>
-                            <RandomRactangle width={17} 
-                                            height={29} imageURL='https://picsum.photos/280/360'/>
-                            <RandomRactangle width={17} 
-                                            height={15} imageURL='https://picsum.photos/320/180'/>
+                            <RandomRactangle width={17} height={33} imageURL='https://picsum.photos/330/360'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>{dummyPlaces[4].placeName}</RactTitle>
+                                    <ReviewPoint>{dummyPlaces[4].reviewPoint}</ReviewPoint>
+                                </SubRactangle>
+                            </RandomRactangle>
+                            <RandomRactangle width={17} height={17} imageURL='https://picsum.photos/330/180'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>미성옥</RactTitle>
+                                </SubRactangle>
+                            </RandomRactangle>
                         </ColumnBox>
                         <ColumnBox>
-                            <RandomRactangle width={27} 
-                                            height={13} imageURL='https://picsum.photos/380/230'/>
-                            <RandomRactangle width={27} 
-                                            height={32} imageURL='https://picsum.photos/320/390'/>
+                            <RandomRactangle width={27} height={16} imageURL='https://picsum.photos/500/230'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>미성옥</RactTitle>
+                                </SubRactangle>
+                            </RandomRactangle>
+                            <RandomRactangle width={27} height={34} imageURL='https://picsum.photos/500/390'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>미성옥</RactTitle>
+                                </SubRactangle>
+                            </RandomRactangle>
                         </ColumnBox>
-                        <RandomRactangle width={24} 
-                                         height={45} imageURL='https://picsum.photos/300/400'/>
+                        <RandomRactangle width={24} height={50} imageURL='https://picsum.photos/410/430'>
+                                <SubRactangle width={0} height={7} imageURL="">
+                                    <RactTitle>미성옥</RactTitle>
+                                </SubRactangle>
+                            </RandomRactangle>
                     </RactangleBox>
                 </Box>
             </SearchContainer>
