a997e24eef8df2e89940440ffab6c0b89072b1cf
leehyeyeong <elto789@naver.com>
2023-11-03 15:24:30 +0900

    feat: 로그인 - react-hook-form을 이용 - 아이디, 비밀번호가 입력되고 버튼을 누르면 formData에 저장되고 post 요청 토큰 값, username 로컬스토리지 저장, 로그인여부 true로 변경

--- src/Pages/Auth/LoginPage/Login.tsx
+++ src/Pages/Auth/LoginPage/Login.tsx
@@ -10,11 +10,17 @@ import {
   Input,
   PasswordInput,
   Submit,
+  ErrorMessage,
 } from "../RegisterPage/StyledRegister";
 import img from "../../../img/grape_background.png";
 import logo from "../../../img/logo.png";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { LoginState } from "../../../Atom/Login";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const LoginContainer = styled(LabelContainer)`
   margin-bottom: 30px;
@@ -27,10 +33,42 @@ const RegisterBtn = styled(Submit)`
   margin-top: 10px;
 `;
 
+interface ILogin {
+  username: string;
+  password: string;
+}
+
 export const LoginPage = () => {
   const navigate = useNavigate();
   const goRegisterPage = () => {
     navigate("/RegisterPage");
+  };
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginState);
+  const formData = useRef<ILogin>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILogin>();
+  const onValid = (data: ILogin) => {
+    formData.current = {
+      username: data.username,
+      password: data.password,
+    };
+    axios
+      .post("http://localhost:8080/api/auth/signin", formData.current)
+      .then((response) => {
+        //console.log(response);
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("username", response.data.username);
+        setIsLoggedIn(true);
+        alert("환영합니다!");
+        navigate("/");
+      })
+      .catch(({ response }) => {
+        //console.log(response.data.error);
+        alert("아이디와 비밀번호를 확인해주세요.");
+      });
   };
   return (
     <>
@@ -41,14 +79,24 @@ export const LoginPage = () => {
         </LogoContainer>
         <Container>
           <Title>Sign in</Title>
-          <Form>
+          <Form onSubmit={handleSubmit(onValid)}>
             <LoginContainer>
               <Label>User ID</Label>
-              <Input placeholder="User ID" />
+              <Input
+                {...register("username", { required: "User ID를 입력하세요." })}
+                placeholder="User ID"
+              />
+              <ErrorMessage>{errors?.username?.message}</ErrorMessage>
             </LoginContainer>
             <LoginContainer>
               <Label>Password</Label>
-              <PasswordInput placeholder="Password" />
+              <PasswordInput
+                {...register("password", {
+                  required: "Password를 입력하세요.",
+                })}
+                placeholder="Password"
+              />
+              <ErrorMessage>{errors?.password?.message}</ErrorMessage>
             </LoginContainer>
             <HomeBtn type="submit" value="G0 TO GRAPE PLATE" />
             <RegisterBtn
