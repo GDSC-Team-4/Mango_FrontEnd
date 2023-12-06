adecad06a0d2aefc3a545bfaa710489295471f46
OhHyukJun <kmh7277@naver.com>
2023-12-06 17:31:14 +0900

    Feat: api 테스트 중

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,10 +1,20 @@
-import { MainHeader } from "./MainHeader";
+import { useRecoilValue } from 'recoil';
+import { searchStateTest } from '../../Atom/Search';
+import { MainHeader } from './MainHeader';
 
 export const MainPage = () => {
+  const searchResults = useRecoilValue(searchStateTest);
+
   return (
     <>
       <MainHeader />
       <p>메인 페이지</p>
+      {/* 배열인지 확인한 후 검색 결과 출력 */}
+      {Array.isArray(searchResults) && searchResults.slice(0, 2).map((result) => (
+        <div key={result.id}>
+          <img src={result.imageURL} alt={result.place_name} />
+        </div>
+      ))}
     </>
   );
 };
