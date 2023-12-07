02679dafec2e8deb3b2060c63912c2c60d639c08
OhHyukJun <kmh7277@naver.com>
2023-12-07 17:50:12 +0900

    Feat: 메인 화면 출력 테스트

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -12,7 +12,8 @@ export const MainPage = () => {
       {/* 배열인지 확인한 후 검색 결과 출력 */}
       {Array.isArray(searchResults) && searchResults.slice(0, 2).map((result) => (
         <div key={result.id}>
-          <img src={result.imageURL} alt={result.place_name} />
+          <p>{result.address_name}</p>
+          <p>{result.place_name}</p>
         </div>
       ))}
     </>
