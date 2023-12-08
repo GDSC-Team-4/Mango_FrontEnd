2aa34a424293775e270db856b4b0a2aecc8cf671
OhHyukJun <kmh7277@naver.com>
2023-12-08 16:17:25 +0900

    Feat: recoilpersist 추가, 검색 키워드 저장 코드 추가

--- src/Atom/Search.ts
+++ src/Atom/Search.ts
@@ -1,6 +1,8 @@
 import { atom, selector } from "recoil";
 import { SearchDto, SearchData, SearchResult } from "../Interface/Search";
+import { recoilPersist } from "recoil-persist";
 
+const { persistAtom } = recoilPersist();
 export const searchState = atom<SearchDto[]>({
     key: "searchState",
     default: [],
@@ -15,5 +17,11 @@ export const searchDataState = atom<SearchData>({
 
 export const searchStateTest = atom<SearchResult[]>({
     key: 'searchStateTest',
-    default: [],  // 초기 상태는 빈 배열
+    default:[],  // 초기 상태는 빈 배열
+    effects_UNSTABLE: [persistAtom],
+  });
+  
+  export const searchValueState = atom({
+    key: 'searchValueState',
+    default: '',
   });
