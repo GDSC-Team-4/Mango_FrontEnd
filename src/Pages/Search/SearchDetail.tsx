c9870a74b5656a0e7b55185c36d931ece83d8f6a
OhHyukJun <kmh7277@naver.com>
2023-12-12 17:33:17 +0900

    Feat: 상세 페이지 코드 작성 중

--- src/Pages/Search/SearchDetail.tsx
+++ src/Pages/Search/SearchDetail.tsx
@@ -1,71 +1,54 @@
 import React,{useRef} from "react";
-import { useRecoilState} from "recoil";
+import { useRecoilState,useRecoilValue} from "recoil";
 import { useNavigate } from "react-router-dom";
 import { dummyPlaces } from "./SearchDummy";
-import { searchDataState,searchStateTest,searchValueState } from "../../Atom/Search";
+import { selectedRestaurantState ,searchStateTest,searchValueState } from "../../Atom/Search";
 import axiosInstance from "../../Api/axios";
-import { SearchContainer, Box, TextBox,
-         SearchBox, SearchIcon, SearchBar,
-         SearchButton, SubTitle, RactangleBox,
-         ColumnBox, RandomRactangle, SubRactangle,
-         RactTitle, ReviewPoint} from "./SearchStyle";
-import { dimensions } from "./SearchDummy";
+import { DetailContainer,Box,DetailText,DetailTitle,DetailView,
+        SubText, DetailImage,ClickLinkText,ReviewView,DetailImages} from "./DetailStyle";
 
 export const SearchDetailPage = () => {
-    const searchRef = useRef<HTMLInputElement>(null);
-    const navigate = useNavigate();
-    const [searchData, setSearchData] = useRecoilState(searchDataState);
-    const [searchResults, setSearchResults] = useRecoilState(searchStateTest);
-    const [searchValue, setSearchValue] = useRecoilState(searchValueState);
-
-    const searchButtonClick = async (event: React.MouseEvent) => {
-        event.stopPropagation();
-        if(searchRef.current) {
-          const searchValue = searchRef.current.value;
-          if (!searchValue.trim()) {
-            alert('검색어를 입력해주세요.');
-            return;
-          }
-          setSearchValue(searchValue);
-          try {
-            setSearchData({
-              ...searchData,
-              SearchPrams: searchValue,
-            });
-            const response = await axiosInstance.get(`map/search?query=${searchValue}`);
-            setSearchResults(response.data.data.documents);
-            console.log(response.data.data.documents);
-            searchRef.current.value='';
-            //navigate('/SearchPage');  
-          } catch (error) {
-            console.error('오류가 발생했습니다: ', error); 
-          }
-        }   
-      }
-        
-      const handleSearchClick = () => {
-        if(searchRef.current) {
-          searchRef.current.focus();
-        }     
-      }
-
+    const selectedRestaurant = useRecoilValue(selectedRestaurantState);
+    const parking = Math.random() < 0.5 ? '주차공간 없음' : '주차공간 있음';
+    console.log(selectedRestaurant);
+    
     return (
         <>
-            <SearchContainer>
-                <Box>
-                    <TextBox>
-                    지역, 식당 또는 음식으로 검색해 나만의 맛집을 찾아보세요!
-                    </TextBox>
-                    <SearchBox onClick={handleSearchClick}>
-                        <SearchIcon/>
-                        <SearchBar type="text" placeholder="지역, 식당 또는 음식" ref={searchRef}/>
-                        <SearchButton onClick={searchButtonClick}>검색</SearchButton>
-                    </SearchBox>
-                    <SubTitle>
-                        "{searchValue}"
-                    </SubTitle>
-                </Box>
-            </SearchContainer>
+            <DetailContainer>
+              <Box>
+                  <DetailTitle>
+                    {selectedRestaurant?.place_name}
+                  </DetailTitle>
+                  <DetailText>
+                    {selectedRestaurant?.address_name}
+                  </DetailText>
+                  <DetailView/>
+                  <DetailText>
+                  <SubText>전화 번호</SubText> 
+                    {selectedRestaurant?.phone}
+                  </DetailText>
+                  <DetailText>
+                  <SubText>카테 고리</SubText> 
+                    {selectedRestaurant?.category_group_name}
+                  </DetailText>
+                  <DetailText>
+                  <SubText>가게 종류</SubText> 
+                    {selectedRestaurant?.category_name}
+                  </DetailText>
+                  <DetailText onClick={()=>window.open(selectedRestaurant?.place_url,"_blank")}>
+                    <SubText>가게 링크</SubText> 
+                    <ClickLinkText>{selectedRestaurant?.place_url}</ClickLinkText>
+                  </DetailText>
+                  <DetailText>
+                  <SubText>주차 공간</SubText> 
+                    {parking}
+                  </DetailText>
+                  <ReviewView/>
+                  <DetailImages/>
+                  
+              </Box>
+              <DetailImage/>
+            </DetailContainer>
         </>
     );
 };
