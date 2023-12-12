9e8074d3ef08af99f6365afd6416a407417c2ea1
leehyeyeong <elto789@naver.com>
2023-12-12 10:28:03 +0900

    feat: 슬라이더 추가

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,6 +1,7 @@
-import { useRecoilValue } from 'recoil';
-import { searchStateTest } from '../../Atom/Search';
-import { MainHeader } from './MainHeader';
+import { useRecoilValue } from "recoil";
+import { searchStateTest } from "../../Atom/Search";
+import { MainHeader } from "./MainHeader";
+import { SimpleSlider } from "./Slider";
 
 export const MainPage = () => {
   const searchResults = useRecoilValue(searchStateTest);
@@ -9,13 +10,15 @@ export const MainPage = () => {
     <>
       <MainHeader />
       <p>메인 페이지</p>
+      <SimpleSlider />
       {/* 배열인지 확인한 후 검색 결과 출력 */}
-      {Array.isArray(searchResults) && searchResults.slice(0, 2).map((result) => (
-        <div key={result.id}>
-          <p>{result.address_name}</p>
-          <p>{result.place_name}</p>
-        </div>
-      ))}
+      {Array.isArray(searchResults) &&
+        searchResults.slice(0, 2).map((result) => (
+          <div key={result.id}>
+            <p>{result.address_name}</p>
+            <p>{result.place_name}</p>
+          </div>
+        ))}
     </>
   );
 };
