fade5bca611c1f1aae697e53052808d5bd7e70e6
LEEHYEYEONG <elto789@naver.com>
2023-12-19 08:59:16 +0900

    ui: 메인화면 슬라이더 작업

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -2,23 +2,45 @@ import { useRecoilValue } from "recoil";
 import { searchStateTest } from "../../Atom/Search";
 import { MainHeader } from "./MainHeader";
 import { SimpleSlider } from "./Slider";
+import axios from "axios";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 95vh;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  margin: 0 auto;
+  gap: 50px;
+  white-space: pre;
+`;
 
 export const MainPage = () => {
   const searchResults = useRecoilValue(searchStateTest);
-
+  axios
+    .get("http://34.64.77.143:8080/main")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   return (
     <>
       <MainHeader />
-      <p>메인 페이지</p>
-      <SimpleSlider />
+      <Wrapper>
+        <SimpleSlider />
+      </Wrapper>
+
       {/* 배열인지 확인한 후 검색 결과 출력 */}
-      {Array.isArray(searchResults) &&
+      {/* {Array.isArray(searchResults) &&
         searchResults.slice(0, 2).map((result) => (
           <div key={result.id}>
             <p>{result.address_name}</p>
             <p>{result.place_name}</p>
           </div>
-        ))}
+        ))} */}
     </>
   );
 };
--- src/Pages/Main/Slider.tsx
+++ src/Pages/Main/Slider.tsx
@@ -1,37 +1,53 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
+
+const Box = styled.div`
+  background-color: black;
+  width: 350px;
+  height: 300px;
+  border-radius: 10px;
+`;
 
 export const SimpleSlider = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <div>
       <h2> Single Item</h2>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+          <Box key={i}>{i}</Box>
+        ))}
       </Slider>
     </div>
   );
