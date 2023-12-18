d5c5c03f80ec469cd3f62d0f3971f83546894396
OhHyukJun <kmh7277@naver.com>
2023-12-18 16:40:03 +0900

    Chore: 리뷰페이지 상수 선언

---
+++ src/Pages/Review/ReviewValue.ts
@@ -0,0 +1,7 @@
+export const starRatings = [
+    { value: 5, text: '매우 맛있어요' },
+    { value: 4, text: '맛있어요' },
+    { value: 3, text: '보통이에요' },
+    { value: 2, text: '별로에요' },
+    { value: 1, text: '실망이에요' },
+  ];
