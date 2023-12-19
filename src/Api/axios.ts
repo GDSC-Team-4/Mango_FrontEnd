1318301d02acb0ce5945f1f4de311bc57df0b420
OhHyukJun <kmh7277@naver.com>
2023-12-19 16:56:32 +0900

    Style: Server_1 -> server

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { SERVER,SERVER_1 } from "../config";
+import { SERVER } from "../config";
 
 const axiosInstance = axios.create({
   //baseURL: `${SERVER.SERVER}`,
-  baseURL: `${SERVER_1.SERVER}`,
+  baseURL: `${SERVER.SERVER}`,
   withCredentials: true,
 });
 
