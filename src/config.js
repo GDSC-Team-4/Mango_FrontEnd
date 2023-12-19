dd8b57b74ff3b4b35d79d4aa945907f845b166e0
OhHyukJun <kmh7277@naver.com>
2023-12-20 00:04:32 +0900

    refactor

--- src/config.js
+++ src/config.js
@@ -1,9 +1,4 @@
-export const SERVER= {
-    SERVER_API:"http://35.216.0.111:8080/api",
-    SERVER:"http://35.216.0.111:8080/"
-    }
-
 export const SERVER_1 = {
-    SERVER_API:"http://35.216.0.111:8080/api",
-    SERVER:"http://34.64.77.143:8080/"
+    SERVER_API:"",
+    SERVER:""
 }
