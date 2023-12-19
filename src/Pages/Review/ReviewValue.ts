f8a076ac3b835bc475d84325289d7d0beb923bef
OhHyukJun <kmh7277@naver.com>
2023-12-19 12:07:33 +0900

    Chore: 상수 수정

--- src/Pages/Review/ReviewValue.ts
+++ src/Pages/Review/ReviewValue.ts
@@ -1,7 +1,7 @@
 export const starRatings = [
-    { value: 5, text: '매우 맛있어요' },
-    { value: 4, text: '맛있어요' },
-    { value: 3, text: '보통이에요' },
-    { value: 2, text: '별로에요' },
-    { value: 1, text: '실망이에요' },
+    { value: 5.0, text: '매우 맛있어요' },
+    { value: 4.0, text: '맛있어요' },
+    { value: 3.0, text: '보통이에요' },
+    { value: 2.0, text: '별로에요' },
+    { value: 1.0, text: '실망이에요' },
   ];
