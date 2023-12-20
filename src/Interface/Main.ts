338e2a66dbcf8ebe7e26efd7a0acac5a058ee410
LEEHYEYEONG <elto789@naver.com>
2023-12-20 15:40:56 +0900

    feat: interface, atom 추가

---
+++ src/Interface/Main.ts
@@ -0,0 +1,13 @@
+export interface MainDto {
+    id: number;
+    placeName: string;
+    categoryName: string;
+    categoryGroupCode: string;
+    phone: string;
+    addressName: string
+    roadAddressName: string;
+    x: string;
+    y: string
+    placeUrl: string;
+    imageUrl: string;
+}
